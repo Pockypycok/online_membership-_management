4748cfea5baf3add5ef557a0d76b5ed8f5523ff8
Pockypycok <cheophy8@gmail.com>
2025-10-02 08:37:04 +0700

    Update user.h

--- user.h
+++ user.h
@@ -1,1 +1,14 @@
+#ifndef USER_H
+#define USER_H
 
+#define NAME_LEN 64
+#define ADDR_LEN 128
+#define PHONE_LEN 32
+
+typedef struct {
+    char name[NAME_LEN];
+    char address[ADDR_LEN];
+    char phone[PHONE_LEN];
+} User;
+
+#endif
